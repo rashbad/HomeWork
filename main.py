2e32fa01c31e57e2134045bfd6ffa9f922329272
Rashad <avtoritetli@vk.com>
2024-10-06 15:57:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# "1st program"
+i = 9 ** 0.5 * 5
+print(i)
+
+# "2nd program"
+a = 9.99 > 9.98 and (1000 != 1000.1)
+print(a)
+
+# "3rd program"
+a = 2 * 2 + 2
+b = 2* (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+# "4th program"
+a = ('123.456')
+print(a[4])
+a = float('123.456')
+num= a * 10
+print(num)
+num = int(num)
+print(num)
+b = num % 10
+print(b)
+
+
+
